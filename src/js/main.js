a175bdab89e0ff2ceb6277d1b3f4bf68412cbd46
IliaMelinchyk <sm1tphoen1x@gmail.com>
2020-12-13 00:21:01 +0300

    nav2

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
-const menuBtn = document.querySelector(".nav__btn"),
-  nav = document.querySelector(".nav");
-menuBtn.addEventListener("click", () => {
-  menuBtn.classList.toggle("open");
-  menuBtn.classList.toggle("nav__background");
+const navBtn = document.querySelector(".nav__btn"),
+  navBackground = document.querySelector(".nav__background"),
+  navLinks = document.querySelector(".nav__links");
+navBtn.addEventListener("click", () => {
+  navBtn.classList.toggle("open");
+  navBackground.classList.toggle("nav__background-window");
+  navLinks.classList.toggle("nav__show");
 });
