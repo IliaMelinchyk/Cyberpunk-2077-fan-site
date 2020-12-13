8586c6e7dc1a8448e3de23472b6517c80594b3ea
IliaMelinchyk <sm1tphoen1x@gmail.com>
2020-12-13 23:00:08 +0300

    before lazy images

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,38 @@ navBtn.addEventListener("click", () => {
   navBackground.classList.toggle("nav__background-window");
   navLinks.classList.toggle("nav__show");
 });
+
+const allSections = document.querySelectorAll(".section");
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+const imgTargets = document.querySelectorAll("img[data-src]");
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  const imgSrc = entry.target.src;
+  console.log(imgSrc);
+  console.log(imgSrc.replace("-lazy", ""));
+  entry.target.src = entry.target.src.replace("-lazy", "");
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+  observer.unobserve(entry.target);
+};
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+});
+imgTargets.forEach((img) => imgObserver.observe(img));
