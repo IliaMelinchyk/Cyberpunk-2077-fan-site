a6a5dde2c586bac57c45c5f74acb5da0ed75b7b4
IliaMelinchyk <sm1tphoen1x@gmail.com>
2020-12-15 16:43:12 +0300

    added loading animation

--- src/js/main.js
+++ src/js/main.js
@@ -98,3 +98,14 @@ const sliderChange = function () {
   });
 };
 sliderChange();
+
+import gsap from "gsap";
+const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
+tl.to("body", 0, { className: "body-hidden" });
+tl.to(".intro__text", { y: "0%", duration: 1, stagger: 0.3 });
+tl.to(".intro__slider", { y: "-100%", duration: 1.5, delay: 0.5 });
+tl.to(".intro", { y: "-100%", duration: 1 }, "-=1.1");
+tl.to("body", 0, { className: "" });
+tl.fromTo(".header__v", { x: "-150%" }, { x: "0%", duration: 1 }, "-=0.9");
+tl.fromTo(".header__video", { x: "150%" }, { x: "0%", duration: 1 }, "-=0.9");
+tl.fromTo(".header__logo", { x: "-150%" }, { x: "0%", duration: 1 }, "-=0.9");
