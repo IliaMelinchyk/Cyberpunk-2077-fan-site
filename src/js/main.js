29ae48315f98a98fb560a152b376ed25befa368e
IliaMelinchyk <sm1tphoen1x@gmail.com>
2020-12-18 04:20:53 +0300

    added responsive vh2

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,10 @@ import sliderChange from "./sliderChange";
 import mobileCheck from "./mobileCheck.js";
 import introAnimation from "./gsapAnimation";
 
-let vh = window.innerHeight * 0.01;
-document.querySelector("header").style.setProperty("--vh", `${vh}px`);
+// let vh = window.innerHeight * 0.01;
+// document.querySelector("header").style.setProperty("--vh", `${vh}px`);
 window.addEventListener("resize", () => {
-  let vh = window.innerHeight * 0.01;
-  document.querySelector("header").style.setProperty("--vh", `${vh}px`);
+  document
+    .querySelector("header")
+    .style.setProperty("--vh", `${window.innerHeight / 100}px`);
 });
